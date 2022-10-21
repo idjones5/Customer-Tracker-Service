e97e496933f73acf85367e9f229b19b422320a1d
idjones5 <97227301+idjones5@users.noreply.github.com>
2022-10-20 17:52:24 -0700

    Maven files.

---
+++ module-9-challenge/src/main/java/com/company/module9challenge/Module9ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.company.module9challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module9ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module9ChallengeApplication.class, args);
+	}
+
+}
---
+++ module-9-challenge/src/test/java/com/company/module9challenge/Module9ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.module9challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module9ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
